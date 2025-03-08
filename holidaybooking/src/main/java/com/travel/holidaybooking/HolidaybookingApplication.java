338ef6936455b1e792572ee1d2740df2388d6fbb
Kushal B S <kushal.basapathi@gmail.com>
2025-03-08 21:26:29 +0000

    Initial commit - Spring Boot setup with MySQL

---
+++ holidaybooking/src/main/java/com/travel/holidaybooking/HolidaybookingApplication.java
@@ -0,0 +1,13 @@
+package com.travel.holidaybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaybookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaybookingApplication.class, args);
+	}
+
+}
